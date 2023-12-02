b9dd81d816beb87d0763f9acdacf61935691c0c3
interrrp <int@addictedto.coffee>
2023-12-02 08:49:57 +0800

    2023 - Day 1 - Part 2

--- 2023/1/main.ts
+++ 2023/1/main.ts
@@ -1,8 +1,21 @@
+const input = await Deno.readTextFile("2023/1/input.txt");
+
 function isDigit(char: string): boolean {
   return /\d/.test(char);
 }
 
 function getDigits(s: string): number[] {
+  s = s
+    .replaceAll("one", "one1one")
+    .replaceAll("two", "two2two")
+    .replaceAll("three", "three3three")
+    .replaceAll("four", "four4four")
+    .replaceAll("five", "five5five")
+    .replaceAll("six", "six6six")
+    .replaceAll("seven", "seven7seven")
+    .replaceAll("eight", "eight8eight")
+    .replaceAll("nine", "nine9nine");
+
   return s
     .split("")
     .filter(isDigit)
@@ -14,10 +27,9 @@ function getFirstAndLastDigits(s: string): [number, number] {
   return [digits[0], digits[digits.length - 1]];
 }
 
-const input = await Deno.readTextFile("2023/1/input.txt");
-let sum = 0;
-for (const line of input.split("\n")) {
-  const [firstDigit, lastDigit] = getFirstAndLastDigits(line);
-  sum += Number(`${firstDigit}${lastDigit}`);
-}
+const sum = input
+  .split("\n")
+  .map((line) => getFirstAndLastDigits(line))
+  .map(([first, last]) => Number(`${first}${last}`))
+  .reduce((prev, curr) => prev + curr);
 console.log(sum);
